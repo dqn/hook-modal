97dfd48495ff303f009d8f06813fcff05a989bd5
dqn <dqn270@gmail.com>
2022-01-21 16:52:28 +0900

    chore: update example

---
+++ example/src/Modal/Modal.tsx
@@ -0,0 +1,25 @@
+import { createPortal } from "react-dom";
+import { useModal } from "../../../src";
+import classes from "./Modal.module.css";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export const Modal: React.FC<Props> = ({ isOpen, onClose, children }) => {
+  const modal = useModal({ onClose });
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={classes.overlay}>
+      <div {...modal} className={classes.content}>
+        {children}
+      </div>
+    </div>,
+    document.body,
+  );
+};
